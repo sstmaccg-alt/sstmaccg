477dcb4c0bb717ef7707bb1490e3054602dbfc07
sstmaccg-alt <sstmaccg@gmail.com>
2026-09-21 13:25:21 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // descartar o cache antigo. Mesmo assim, o index.html agora é buscado na
 // rede primeiro (veja abaixo), então atualizações aparecem sozinhas.
 
-const CACHE_NAME = 'sst-ccg-v25';
+const CACHE_NAME = 'sst-ccg-v26';
 const ARQUIVOS_PARA_CACHE = [
   './',
   './index.html',
